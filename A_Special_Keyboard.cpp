89affaa0e95ce936a0e6a0d680af769e67d4a57b
Varun Yadav <varunyadav891@gmail.com>
2022-06-03 23:32:10 +0530

    added

---
+++ A_Special_Keyboard.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(string s1 , string s2){
+	map<char,int>mp;
+
+	for(int i = 0; i<s1.size(); i++){
+		mp.insert({s1[i],i}); // in this way we can store the index corresponding to char
+	}
+	int last = 0;
+	int total = 0;
+
+	for(int i = 0; i<s2.size(); i++){
+		total += abs(mp[s2[i]] - last);
+		last = mp[s2[i]]; //this will give the index of the character 
+	}
+
+	return total;
+}
+
+int main(){
+
+	string s1 = "abcdefghijklmnopurstuvwxyz";
+	string s2 = "cba";
+
+	int ans = solve(s1,s2);
+	cout<<ans<<endl;
+	return 0;
+}
