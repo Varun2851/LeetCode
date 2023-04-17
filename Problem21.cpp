c7a3fc0cfe507353f075131e2bfb22f0193e3f3c
Varun Yadav <varunyadav891@gmail.com>
2023-04-17 22:20:43 +0530

    added

---
+++ Problem21.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BubbleSort(int *arr, int n){
+
+	int globalCount = 0;
+
+	for(int i = 0; i<n-1; i++){
+		int cnt = 0;
+		for(int j = 0; j<=n-2-i; j++){
+			if(arr[j]>arr[j+1]){
+				cnt++;
+				globalCount++;
+				swap(arr[j],arr[j+1]);
+			}
+		}
+		if(cnt == 0){
+			break;
+		}
+	}
+	return globalCount;
+}
+
+int main(){
+
+	//int arr[] = {123, 21, 34, 45, 25, 675, 23, 44, 55, 900};
+	int arr[] = {2};
+	int n = sizeof(arr)/sizeof(int);
+
+	cout<<BubbleSort(arr,n);
+	return 0;
+}
