28ff479749eda694774f19a1311ffeef4c4bb0e6
Varun Yadav <varunyadav891@gmail.com>
2023-04-28 23:49:08 +0530

    added

---
+++ MaximumSubarrayLength.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int *arr,int n, int k){
+	int count = 0;
+	queue<int>q;
+	int sum = 0;
+	for(int right = 0; right<n; right++){
+		sum += arr[right];
+		q.push(arr[right]);
+
+		while(sum<=k){
+			int x = q.front();
+			q.pop();
+			k = k -x;
+			count += q.size();
+		}
+		
+	}
+	return count;
+}
+
+int main(){
+	int arr[] = {2,5,6};
+	int n = 3;
+	int k = 10;
+	cout<<solve(arr,n,k);
+	return 0;
+}
