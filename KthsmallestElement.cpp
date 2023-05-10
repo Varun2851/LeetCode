fc51dc9d17094977e9c0ebd398bf8e52d2b58cbb
Varun Yadav <varunyadav891@gmail.com>
2023-05-10 22:30:19 +0530

    added

---
+++ KthsmallestElement.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int *arr,int n, int k){
+	int low = arr[0];
+	int high = arr[n-1];
+
+	while(low<high){
+		int count =  0;
+		int mid = low+(high-low)/2;
+
+		count = upper_bound(arr,arr+n,mid)-arr;
+		
+		if(count<k){
+			low = mid+1;
+		}
+		else{
+			high = mid;
+		}
+	}
+	return low;
+}
+
+int main(){
+	int arr[] = {1,2,3,10,12,15,18};
+	int n = sizeof(arr)/sizeof(int);
+	int k = 4;
+
+	cout<<solve(arr,n,k);
+
+	return 0;
+}
---
+++ ReverseStack.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void InsertAtBottom(stack<int>&s,int topElement){
+
+	//base case
+	if(s.empty()){
+		s.push(topElement);
+		return;
+	}
+	//recursive case
+	int top = s.top();
+	s.pop();
+	InsertAtBottom(s,topElement);
+	s.push(top);
+}
+
+
+void Reverse(stack<int>&s){
+	//base case
+	if(s.empty()){
+		return;
+	}
+	//recursive case
+	int topElement = s.top();
+	s.pop();
+	Reverse(s);
+	InsertAtBottom(s,topElement);
+
+}
+
+void Print(stack<int>s){ //dont send stack call by reference in print function
+
+	while(!s.empty()){
+		cout<<s.top()<<" ";
+		s.pop();
+	}
+}
+
+int main(){
+
+	stack<int>s;
+
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	s.push(4);
+
+	Print(s);
+	Reverse(s);
+	Print(s);
+
+	return 0;
+
+}
