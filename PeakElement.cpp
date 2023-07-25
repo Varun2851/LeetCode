01c1f9ddfd2e298fc56bbde86cb89a0791357d4e
Varun Yadav <varunyadav891@gmail.com>
2023-07-25 22:33:04 +0530

    added

---
+++ PeakElement.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int index = 0;
+        
+        if(n >= 3){
+            
+            for(int i = 1; i<=n-2;){
+                
+                // if the ith element is peak or not
+                if(arr[i] > arr[i+1] and arr[i] > arr[i-1]){
+                    index = i;
+                    i++;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return index ;
+    }
+};
